e974837c9c653a053254f873bbfd80023c6649ca
lcs1001 <lcs1001@alu.ubu.es>
2019-12-17 17:15:15 +0100

    Cambios

--- TresEnRaya/Servidor/IntermediarioServidor.py
+++ TresEnRaya/Servidor/IntermediarioServidor.py
@@ -1,40 +1,59 @@
-def arbitrar(self, msg, elem=None):
+#Imports
+from #TODO importat arbitro
+
+"""
+Clase de Capa de Comunicaciones.
+
+Encargada de llamar a las funciones de Árbitro dependiendo 
+de los mensajes recibidos del Servidor.
+"""
+class IntermediarioServidor:
+
+     def __init__(self):
         """
-        En función del mensaje recibido se realiza una cierta acción.
-
-        Parámetros:
-        msg -- Mensaje recibido del Cliente
-        elem -- Objeto recibido del Cliente
-
-        Return:
-        fin -- Código del mensaje a devolver en función de la acción realizada
-        obj -- Objeto del mensaje a devolver en función de la acción realizada
-        turno -- Turno actual
+        Método que inicializa el Intermediario del Servidor.
+        Se inicializan las variables.
         """
-        fin = 0
-        # Tablero pintado en InterfazJugador --> solicitarMov
-        if(msg == "101"):
-            fin = "203"
-            obj = "0"
+        self.cod = ''
+        self.obj = ''
 
-        # Solicitar tablero
-        if(msg == "103"):
-            fin, obj = self.dibujarTablero()
+    def arbitrar(self, msg, elem=None):
+            """
+            En función del mensaje recibido se realiza una cierta acción.
 
-        # Movimiento a realizar
-        if(msg == "104"):
-            fin, obj = self.realizarMovimiento(elem)
+            Parámetros:
+            msg -- Mensaje recibido del Cliente
+            elem -- Objeto recibido del Cliente
 
-        # Se ha realizado el movimiento y se ha actualizado el tablero
-        if(msg == '105'):
-            fin = self.esFin()
-            if (fin == self.turno):
-                return "200", str(self.turno), self.turno  # Codigo fin ganando
-            elif (fin == "0"):
-                return "200", "0", self.turno  # Codigo fin empate
+            Return:
+            fin -- Código del mensaje a devolver en función de la acción realizada
+            obj -- Objeto del mensaje a devolver en función de la acción realizada
+            turno -- Turno actual
+            """
+            fin = 0
+            # Tablero pintado en InterfazJugador --> solicitarMov
+            if(msg == "101"):
+                fin = "203"
+                obj = "0"
 
-            self.cambiarTurno()
-            fin, obj = self.dibujarTablero()
+            # Solicitar tablero
+            if(msg == "103"):
+                fin, obj = self.dibujarTablero()
 
-        # Se devuelve el codigo de respuesta, el objeto y el turno
-        return fin, obj, self.turno
+            # Movimiento a realizar
+            if(msg == "104"):
+                fin, obj = self.realizarMovimiento(elem)
+
+            # Se ha realizado el movimiento y se ha actualizado el tablero
+            if(msg == '105'):
+                fin = self.esFin()
+                if (fin == self.turno):
+                    return "200", str(self.turno), self.turno  # Codigo fin ganando
+                elif (fin == "0"):
+                    return "200", "0", self.turno  # Codigo fin empate
+
+                self.cambiarTurno()
+                fin, obj = self.dibujarTablero()
+
+            # Se devuelve el codigo de respuesta, el objeto y el turno
+            return fin, obj, self.turno
